a4f6c6e62e1724781ad5a1fbf4a8cad888e5e3d2
Hassan-M <hassan.ma1245@gmail.com>
2021-01-30 22:57:32 +0330

    create comment, tag, course model

--- web_app/courses/admin.py
+++ web_app/courses/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ['name', 'free_or_paid', 'rate', 'text', 'instructor_rate', 'pace_rate', 'depth_coverage_rate',
+                    'quality_rate', 'number_of_comments', 'number_of_views', 'media_url', 'medium', 'excersice']
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['course_id', 'parent', 'rate', 'text']
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['name']
--- web_app/courses/models.py
+++ web_app/courses/models.py
@@ -1,3 +1,35 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
+
+class Course(models.Model):
+    name = models.TextField()
+    TYPES = [('P', 'Paid'), ('F', 'Free')]
+    free_or_paid = models.CharField(choices=TYPES, max_length=1, blank=True)
+    rate = models.IntegerField(default=0)
+    text = models.TextField(blank=True)
+    instructor_rate = models.IntegerField(default=0)
+    pace_rate = models.IntegerField(default=0)
+    depth_coverage_rate = models.IntegerField(default=0)
+    quality_rate = models.IntegerField(default=0)
+    number_of_comments = models.PositiveIntegerField(default=0)
+    number_of_views = models.PositiveIntegerField(default=0)
+    media_url = models.URLField(blank=True, null=True)
+    medium = models.CharField(max_length=100)
+    excersice = models.BooleanField(default=False)
+    tags = models.ManyToManyField(Tag, blank=True)
+    submitter = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+
+class Comment(models.Model):
+    course_id = models.ForeignKey(Course, on_delete=models.CASCADE)
+    parent = models.ForeignKey("Comment", on_delete=models.CASCADE, blank=True, null=True)
+    rate = models.IntegerField(default=0)
+    text = models.TextField()
+    submitter = models.ForeignKey(User, on_delete=models.CASCADE)
+
